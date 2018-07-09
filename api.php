9ffa61a8dae7dc33240b604c8eaed27247d93c74
Zeynel Akçay <zeynelakcay@Zeynel-MacBook-Air.local>
2018-07-09 15:47:27 +0300

    Açıklama Eklendi

--- api.php
+++ api.php
@@ -1,16 +1,16 @@
 <?php
 
 header('Content-Type: text/html; charset=utf-8');
-$company_id	 = $COMPANY_ID;
-$website_id	 = $WEBSITE_ID;
-$order_status_id = $ORDER_STATUS_ID;
+$company_id	 = $COMPANY_ID; // Panel Üzerinden Elde Edilecek
+$website_id	 = $WEBSITE_ID; // Panel Üzerinden Elde Edilecek
+$order_status_id = $ORDER_STATUS_ID; // Panel Üzerinden Elde Edilecek
 
 (isset($_POST['fullname'])) ? $fullname	 = $_POST['fullname'] : "Name Not Entered";
 (isset($_POST['email'])) ? $email		 = $_POST['email'] : "E-mail Not Entered";
 (isset($_POST['phone'])) ? $phone		 = $_POST['phone'] : "Phone Not Entered";  
 
 
-$message = $MESSAGE_TEXT;
+$message = $MESSAGE_TEXT; // Formdan Gelen Diger Bilgiler
 
 $url	 = 'http://kolaysatisyonetimi.com/Api/Order/add';
 $data	 = array('company_id' => $company_id, 'website_id' => $website_id, 'order_status_id' => $order_status_id, 'fullname' => $fullname, 'email' => $email, 'phone' => $phone, 'comment' => $message);
